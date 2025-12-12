f78b75eaebf90b40973548bf6a6df2e34410874a
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2025-12-12 21:03:03 +0530

    Linked list basic operations -> add , delete , reverse , finding middle , loop detection/deletion

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -175,3 +175,46 @@ public class LinkedList1 {
         return slow;
     }
 }
+
+public boolean detectLoop(){
+  Node slow = head;
+  Node fast = head;
+
+  while(fast != null && fast.next != null){
+    slow = slow.next;
+    fast = fast.next.next;
+    if(slow == fast){
+      return true;
+    }
+  }  
+  return false;
+}
+
+
+public void removeLoop(){
+  Node slow = head;
+  Node fast = head;
+  boolean loop = false;
+  while(fast != null && fast.next != null){
+    slow = slow.next;
+    fast = fast.next.next;
+    if(slow == fast){
+      loop = true;
+      break;
+    }
+  }
+  if(loop = false){
+     return;
+   } 
+   if(loop = true){
+     slow = head;
+     Node prev = null;
+     while(slow != fast){
+       prev = fast;
+       slow = slow.next;
+       fast = fast.next;
+     }
+     prev.next = null;
+   }
+}
+
