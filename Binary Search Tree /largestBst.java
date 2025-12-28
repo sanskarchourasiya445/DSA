c96b28996ddd3aa1380a07b4a14496841a16df9e
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2025-12-28 22:25:43 +0530

    Create largestBst.java

---
+++ Binary Search Tree /largestBst.java
@@ -0,0 +1,61 @@
+public class LargestBST{
+  static class Node {
+    int data;
+    Node left, right;
+    
+    Node(int data) {
+      this.data = data;
+    }
+  }
+  
+  static class Info {
+    boolean isBST;
+    int size;
+    int min;
+    int max;
+
+    Info(boolean isBST, int size, int min, int max) {
+      this.isBST = isBST;
+      this.size = size;
+      this.min = min;
+      this.max = max;
+    }
+  }
+  
+  public static int maxBST = 0;
+  
+  public static Info largestBST(Node root) {
+    if (root == null) {
+      return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+    Info left = largestBST(root.left);
+    Info right = largestBST(root.right);
+
+    int size = left.size + right.size + 1;
+    int min = Math.min(root.data, Math.min(left.min, right.min));
+    int max = Math.max(root.data, Math.max(left.max, right.max));
+
+    if( root.data <= left.max || root.data >= right.min){
+      return new Info(true, size, min, max);
+    }
+    
+    if (left.isBST && right.isBST) {
+      maxBST = Math.max(maxBST, size);
+    }
+
+    return new Info(false, size, min, max);
+  }
+  
+  public static void main(String[] args) {
+    Node root = new Node(50);
+    root.left = new Node(30);
+    root.right = new Node(60);
+    root.left.left = new Node(5);
+    root.left.right = new Node(20);
+    root.right.left = new Node(45);
+    root.right.right = new Node(70);
+    root.right.right.right = new Node(80);
+    largestBST(root);
+    System.out.println("Size of Largest BST = " + maxBST);
+    }
+}
