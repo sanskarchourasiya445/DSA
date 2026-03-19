9aa5b9e613b2b8cff12690a7863c6f637f50e97e
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-03-19 15:10:02 +0530

    Add linked list with addFirst method
    Implement a linked list with addFirst method and print functionality.

---
+++ LinkedList/addFirst.java
@@ -0,0 +1,36 @@
+public class AddFirst {
+
+    Node head;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        AddFirst list = new AddFirst();
+
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
+
+        list.print();
+    }
+}
