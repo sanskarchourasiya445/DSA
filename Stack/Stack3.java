1dc21fc7463a514b89f74b5c0a416812a11faec0
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-01 19:13:49 +0530

    Update Stack3.java

--- Stack/Stack3.java
+++ Stack/Stack3.java
@@ -62,16 +62,16 @@ public class StackQ {
     }
   }
   
-  // public static void main(String args[]) {
-  //   Stack s = new Stack();
-  //   s.push(1);
-  //   s.push(2);
-  //   s.push(3);
+  public static void main(String args[]) {
+    Stack s = new Stack();
+    s.push(1);
+    s.push(2);
+    s.push(3);
 
-  //   while (!s.isEmpty()){
-  //     System.out.println(s.peek());
-  //     s.pop();
-  //   }
-  // }
+    while (!s.isEmpty()){
+      System.out.println(s.peek());
+      s.pop();
+    }
+  }
 
 }
