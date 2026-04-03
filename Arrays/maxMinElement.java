14a8aa9178b4dbfa8368e1dc691641c215e0e9ca
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-03 23:14:03 +0530

    Refactor MaxMinElement.java and remove unused classes
    Removed unused classes and cleaned up code.

---
+++ Arrays/maxMinElement.java
@@ -0,0 +1,24 @@
+public class MaxMinElement {
+
+    public static void findMaxMin(int[] arr) {
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 9, 1, 7, 5};
+        findMaxMin(arr);
+    }
+}
