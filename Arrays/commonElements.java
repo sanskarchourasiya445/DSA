240f57850be1d41ee43ef84084861adbc738f988
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-04 18:27:43 +0530

    Add CommonElements class to find common integers
    Implemented a method to find and print common elements between two integer arrays.

---
+++ Arrays/commonElements.java
@@ -0,0 +1,24 @@
+public class CommonElements {
+
+    public static void findCommon(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+            } else if (arr1[i] < arr2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 4, 5, 6};
+        int[] arr2 = {2, 4, 6, 8};
+        findCommon(arr1, arr2);
+    }
+}
