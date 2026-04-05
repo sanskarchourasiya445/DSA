7e450c576e8eabf14d3da932b04e4cf3c27f8904
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-05 18:37:02 +0530

    Add implementation for longest substring with K distinct chars

---
+++ Arrays/longestSubstringKDistinct.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class LongestSubstringKDistinct {
+
+    public static int longestSubstring(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while (map.size() > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestSubstring("eceba", 2));
+    }
+}
