9ed47cdd734a8d224e20ae5028da9c23ce005a81
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-05 18:34:46 +0530

    Implement max sum subarray of size K

---
+++ Arrays/maxSumSubarraySizeK.java
@@ -0,0 +1,24 @@
+public class MaxSumSubarraySizeK {
+
+    public static int maxSum(int[] arr, int k) {
+        int windowSum = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        maxSum = windowSum;
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        System.out.println(maxSum(arr, 3));
+    }
+}
