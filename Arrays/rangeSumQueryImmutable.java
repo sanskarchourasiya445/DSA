1a0261fdfd2a8f727d96ee70724646eaab8da7ea
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-07 18:38:56 +0530

    Add RangeSumQueryImmutable class for range sum queries
    Implement RangeSumQueryImmutable class with prefix sum array for efficient range sum queries.

---
+++ Arrays/rangeSumQueryImmutable.java
@@ -0,0 +1,25 @@
+public class RangeSumQueryImmutable {
+    private int[] prefix;
+
+    public RangeSumQueryImmutable(int[] nums) {
+        prefix = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 0, 3, -5, 2, -1};
+
+        RangeSumQueryImmutable obj = new RangeSumQueryImmutable(nums);
+
+        System.out.println(obj.sumRange(0, 2)); // 1
+        System.out.println(obj.sumRange(2, 5)); // -1
+        System.out.println(obj.sumRange(0, 5)); // -3
+    }
+}
