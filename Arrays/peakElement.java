698bbecb17e434657a1b3f5fc985bf2cb867e2b1
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-10 07:51:38 +0530

    Implement peak element finder in PeakElement class

---
+++ Arrays/peakElement.java
@@ -0,0 +1,23 @@
+public class PeakElement {
+
+    public static int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println("Peak index = " + findPeakElement(nums));
+    }
+}
