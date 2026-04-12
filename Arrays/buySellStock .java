deed9fc00eddd499605d27557956ff7fa6391887
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-04-12 07:48:26 +0530

    Create buySellStock .java

---
+++ Arrays/buySellStock .java
@@ -0,0 +1,19 @@
+public class BestTimeToBuySellStock {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println("Profit = " + maxProfit(prices));
+    }
+}
